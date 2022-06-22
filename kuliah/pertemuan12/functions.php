307c9ed9e31a707665ffe5272b73f4c7f230b515
IqbalDwiNulhakim <iqbaldwinulhakim04@gmail.com>
2022-06-22 23:06:18 +0700

    insert data

--- kuliah/pertemuan12/functions.php
+++ kuliah/pertemuan12/functions.php
@@ -6,11 +6,36 @@ function koneksi()
 
 function query($query){
   $conn = koneksi();
+
   $result = mysqli_query($conn, $query);
+
+  if( mysqli_num_rows($result) == 1){
+    return mysqli_fetch_assoc($result);
+  }
+
   $rows = [];
   while ($row = mysqli_fetch_assoc($result)) {
     $rows[] = $row;
 }
 
   return $rows;
+}
+
+function tambah($data){
+  $conn = koneksi();
+
+  $nama = $data['nama'];
+  $nama = $data['nim'];
+  $nama = $data['email'];
+  $nama = $data['jurusan'];
+  $gambar = $data['gambar'];
+
+  $query = "INSERT INTO
+              mahasiswa
+            VALUES
+            (null, '$nama', '$nim', '$email', '$jurusan', '$gambar');
+            ";
+  mysqli_query($conn, $query);
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
 }
