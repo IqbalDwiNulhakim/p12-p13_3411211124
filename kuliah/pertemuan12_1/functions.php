d49095ed36d72789f3518c5bb5d668e88a13ae43
IqbalDwiNulhakim <iqbaldwinulhakim04@gmail.com>
2022-06-26 12:07:00 +0700

    menambah file dan folder pada pertemuan 13

--- kuliah/pertemuan12_1/functions.php
+++ kuliah/pertemuan12_1/functions.php
@@ -44,4 +44,45 @@ function tambah($data){
 function hapus($id){
   $conn = koneksi();
   mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data){
+  $conn = koneksi();
+
+  $id = $data['id'];
+  $nama = htmlspecialchars($data['nama']);
+  $nim = htmlspecialchars($data['nim']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "UPDATE mahasiswa SET
+            nama = '$nama',
+            nim = '$nim',
+            email = '$email',
+            jurusan = '$jurusan',
+            gambar = '$gambar',
+          WHERE id = $id";
+  mysqli_query($conn, $query);
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword){
+  $conn = koneksi();
+
+  $query = "SELECT * FROM mahasiswa
+              WHERE 
+            nama LIKE'%$keyword%' OR 
+            nim LIKE'%$keyword%' OR
+            email LIKE'%$keyword%' OR
+            jurusan LIKE'%$keyword%'";
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
 }
